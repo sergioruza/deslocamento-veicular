eb3d8550e2d8fa6981fc435e40c86626aa87f07a
Sergio <sergioruza22@gmail.com>
2023-06-19 14:51:28 -0300

    refactor: turns the login form into a component

---
+++ src/components/FormLoginCustomer.tsx
@@ -0,0 +1,49 @@
+import { FieldValues, UseFormRegister, useForm } from 'react-hook-form';
+
+import Select from './/Select';
+import Button from './Button';
+import Input from './Input';
+
+import { yupResolver } from '@hookform/resolvers/yup';
+import schemaLoginCustomer from 'validations/schemaLoginCustomer';
+
+function FormLoginCustomer() {
+  const {
+    register,
+    handleSubmit: onSubmit,
+    formState: { errors }
+  } = useForm({ resolver: yupResolver(schemaLoginCustomer) });
+  const handleSubmit = ({ document, name, typeDocument }: FieldValues) => {
+    console.log(event);
+  };
+
+  return (
+    <form onSubmit={onSubmit(handleSubmit)}>
+      <Input
+        label="Nome"
+        name="name"
+        register={register as unknown as UseFormRegister<FieldValues>}
+        placeholder="Digite seu nome"
+        errors={errors}
+      />
+
+      <Select
+        register={register as unknown as UseFormRegister<FieldValues>}
+        options={['CPF', 'RG']}
+        label="Tipo de Documento"
+      />
+
+      <Input
+        name="document"
+        placeholder="Digite o documento"
+        register={register as unknown as UseFormRegister<FieldValues>}
+        label="Documento"
+        errors={errors}
+      />
+
+      <Button type="submit" actionName="Enviar" />
+    </form>
+  );
+}
+
+export default FormLoginCustomer;
--- src/pages/signin-form/customer.tsx
+++ src/pages/signin-form/customer.tsx
@@ -1,48 +1,10 @@
-import { FieldValues, UseFormRegister, useForm } from 'react-hook-form';
-
-import Button from 'components/Button';
-import Input from 'components/Input';
-import Select from 'components/Select';
-
-import { yupResolver } from '@hookform/resolvers/yup';
-import schemaLoginCustomer from 'validations/schemaLoginCustomer';
+import FormLoginCustomer from 'components/FormLoginCustomer';
 
 function customer() {
-  const {
-    register,
-    handleSubmit: onSubmit,
-    formState: { errors }
-  } = useForm({ resolver: yupResolver(schemaLoginCustomer) });
-  const handleSubmit = (event: FieldValues) => {
-    console.log(event);
-  };
-
   return (
-    <form onSubmit={onSubmit(handleSubmit)}>
-      <Input
-        label="Nome"
-        name="name"
-        register={register as unknown as UseFormRegister<FieldValues>}
-        placeholder="Digite seu nome"
-        errors={errors}
-      />
-
-      <Select
-        register={register as unknown as UseFormRegister<FieldValues>}
-        options={['CPF', 'RG']}
-        label="Tipo de Documento"
-      />
-
-      <Input
-        name="document"
-        placeholder="Digite o documento"
-        register={register as unknown as UseFormRegister<FieldValues>}
-        label="Documento"
-        errors={errors}
-      />
-
-      <Button type="submit" actionName="Enviar" />
-    </form>
+    <div>
+      <FormLoginCustomer />
+    </div>
   );
 }
 
