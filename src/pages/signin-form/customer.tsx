ebdfa9a9e4c3ccd16799b394b7395b35ab875b32
Sergio <sergioruza22@gmail.com>
2023-06-19 10:16:57 -0300

    feat: component button

--- src/pages/signin-form/customer.tsx
+++ src/pages/signin-form/customer.tsx
@@ -1,6 +1,7 @@
 import { FieldValues, useForm } from 'react-hook-form';
 
-import Input from '../../components/Input';
+import Button from 'components/Button';
+import Input from 'components/Input';
 
 function customer() {
   const {
@@ -27,6 +28,8 @@ function customer() {
         register={register}
         label="Documento"
       />
+
+      <Button type="submit" actionName="Enviar" />
     </form>
   );
 }
