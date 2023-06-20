0936c553924a46cb95a773fa813135f2c21cf99d
Sergio <sergioruza22@gmail.com>
2023-06-20 11:06:20 -0300

    feat: implements registration form for driver

---
+++ src/components/FormLoginCunductor.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function FormLoginCunductor() {
+  return <form></form>;
+}
+
+export default FormLoginCunductor;
---
+++ src/pages/signin-form/conductor.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function conductor() {
+  return <div>conductor</div>;
+}
+
+export default conductor;
