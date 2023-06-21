16780b562c1ed94b38e3544a3113045f73080ffd
Sergio <sergioruza22@gmail.com>
2023-06-21 10:08:29 -0300

    feat: delete span component and implement span profile

--- src/components/HomeCustomer.tsx
+++ src/components/HomeCustomer.tsx
@@ -1,10 +1,16 @@
+import { useRouter } from 'next/router';
 import React from 'react';
 
 function HomeCustomer() {
+  function handleCLick(url: string) {
+    const route = useRouter();
+    route.push(url);
+  }
+
   return (
     <div>
       <header>
-        <span></span>
+        <span onClick={() => handleCLick('/profile/customer')}>Perfil</span>
       </header>
     </div>
   );
