a882483e3ca7faac79a55977d31015cef8acd670
Sergio <sergioruza22@gmail.com>
2023-06-24 10:15:45 -0300

    feat: logout function in /profile/customer

--- src/pages/profile/customer.tsx
+++ src/pages/profile/customer.tsx
@@ -1,11 +1,19 @@
+import { useRouter } from 'next/router';
 import React from 'react';
 
+import Button from 'components/Button';
 import FormProfile from 'components/FormProfile';
 
 function customer() {
+  const router = useRouter();
+  function logout() {
+    localStorage.removeItem('user');
+    router.push('/');
+  }
   return (
     <div>
       <FormProfile />
+      <Button actionName="Sair" type="button" onClick={logout} />
     </div>
   );
 }
